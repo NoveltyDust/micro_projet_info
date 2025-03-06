a6597598b19219ad268c241100dcb514187a091e
YJE170 <yassine.jelassi@etu.univ-nantes.fr>
2025-03-06 17:12:16 +0100

    Add files via upload

--- fonction_diagonale.py
+++ fonction_diagonale.py
@@ -1,17 +1,24 @@
+from math import sqrt
+
 def diagonale(poly):
-    a = poly[0]
-    b = poly[1]
-    c = poly[2]
-    d = poly[3]
+    a, b, c, d = poly
     liste_ef = []
     
-    for e in range (1, min(a+b,c+d)):
-        p1 = a + b + e
-        p2 = c + d + e
-        aire_abe = sqrt(p1*(p1-a)*(p1-b)*(p1-e))
-        aire_cde = sqrt(p2*(p2-c)*(p2-d)*(p2-e))
-        aire_abcd = aire_abe +  aire_cde
-        f = sqrt(16 * (aire_abcd)**2 +(b**2+d**2-a**2-b**2)**2)/2*e
-        if 0 < f < min(a+d,b+c) :
-            liste_ef.append((e,f))
+    for e in range(1, min(a + b, c + d)):
+        p1 = (a + b + e) / 2  # Demi-périmètre
+        p2 = (c + d + e) / 2  # Demi-périmètre
+
+        try:
+            aire_abe = sqrt(p1 * (p1 - a) * (p1 - b) * (p1 - e))
+            aire_cde = sqrt(p2 * (p2 - c) * (p2 - d) * (p2 - e))
+            aire_abcd = aire_abe + aire_cde
+
+            f = sqrt(16 * aire_abcd**2 + (b**2 + d**2 - a**2 - c**2)**2) / (2 * e)
+            
+            if 0 < f < min(a + d, b + c):
+                liste_ef.append((e, f))
+        except ValueError:
+            # Gestion des erreurs mathématiques si sqrt reçoit une valeur négative
+            continue  
+
     return liste_ef
