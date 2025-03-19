7c3a6197ecd2995de939b6c36f65ccc4395b2b51
Dusty <novelty.dreem5@gmail.com>
2025-03-19 11:32:09 +0100

    Finalisation de circles.py, et renommage de micro projet.py (pour enlever l'espace)

--- circles.py
+++ circles.py
@@ -1,20 +1,8 @@
 from math import sqrt
 import matplotlib.pyplot as plt
-from fonction_diagonale import diagonale
-import numpy
 
-a = 5
-b = 3
-c = 4
-
-xa = 0
-xb = a
-ya = 0
-yb = 0
-xc = (a**2+c**2-b**2)/(2*a)
-yc = sqrt(c**2-xc**2)
-
-def check_intersections(a):
+def check_intersections(poly):
+    a = max(poly)
     intersections = []
     for i in range(a):
         for j in range(a):
@@ -32,13 +20,6 @@ def visualisation(a=tuple,b=tuple,c=tuple,d=tuple):
     plt.plot([b[0],c[0]],[b[1],c[1]],linestyle="--",color="red")
     plt.plot([c[0],d[0]],[c[1],d[1]],linestyle="--",color="red")
     plt.plot([d[0],a[0]],[d[1],a[1]],linestyle="--",color="red")
-    plt.scatter(*zip(a,b,c),color="red")
-
-
-def belong_to_circles(poly=tuple):
-    L = max(poly)
-    sides = poly.pop(L)
-    diag = diagonale(poly)
-    a = (0,0)
-    b = (L,0)
-    c = (a**2-diag[0]**2)
+    plt.scatter(*zip(a,b,c,d),color="red")
+    print((d[1]-c[1])/(d[0]-c[0]))
+    plt.show()
