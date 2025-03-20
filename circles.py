5a54a3295ef6ea56a8c89c5b0a1f4a1ffd51d77d
Dusty <novelty.dreem5@gmail.com>
2025-03-20 16:53:50 +0100

    changes

--- circles.py
+++ circles.py
@@ -3,33 +3,33 @@ import matplotlib.pyplot as plt
 from fonction_repere import test_repere,afficher_quadrilatere_fix
 from fonction_diagonale import diagonale
 
-def check_intersections(poly):
-    a = max(poly)
+def check_intersections(cote,y):
+    a = cote
     intersections = []
-    for i in range((2*a)+1):
-        for j in range((2*a)+1):
+    for i in range(1,a+1):
+        for j in range(1,a+1):
             xm = (i**2 - j**2 + a**2) / (2 * a)
             ymcar = i**2-xm**2
-            if ymcar >= 0:
+            if ymcar > 0:
                 ym = sqrt(ymcar)
-                intersections.append((xm,ym))
+                if y!=0:
+                    intersections.append((xm,-ym+y))
+                else:
+                    intersections.append((xm,ym+y))
     return intersections
 
-def view_inter(inter, poly):
-    afficher_quadrilatere_fix(poly)
-    fig, ax = plt.subplots()
-    l = max(poly)
-    a=(0,0)
-    b=(l,0)
+def view_inter(inter, cote,y):
+    l = cote
+    a=(0,y)
+    b=(cote,y)
     plt.plot([a[0],b[0]],[a[1],b[1]],color="red")
     plt.scatter(*zip(*inter),color="g")
-    for i in range(1,(2*l)+1):
+    for i in range(1,l+1):
         A = plt.Circle(a,i,color="b",fill=False)
         B = plt.Circle(b,i,color='b',fill=False)
         plt.gca().add_patch(A)
         plt.gca().add_patch(B)
     plt.grid()
-    plt.show()
 
 def compare(poly):
     coords = test_repere(poly)
@@ -40,7 +40,11 @@ def compare(poly):
             if inter==coord[2] or inter==coord[3]:
                 print("appartient auc cercles")
 
-poly = (5,5,6,4)
-inter = check_intersections(poly)
-view_inter(inter,poly)
+poly = (5,6,5,6)
+A = (0,0)
+B = (0,poly[0])
+inter = check_intersections(max(poly),A[1])
+inter2 = check_intersections(max(poly),B[1])
+view_inter(inter,max(poly),A[1])
+view_inter(inter2,max(poly),B[1])
 plt.show()
